fc9a911ed0cd089a4680df7ce3c74370a4bbcc70
aissa <aissa@172.17.205.59>
2022-10-07 09:04:13 +0200

    fin td

---
+++ src/Personnages/Village.java
@@ -0,0 +1,18 @@
+package Personnages;
+
+public class Village {
+	private Gaulois[] villageois;
+	private  int nbvillageois = 0;
+	private String nom;
+	private Chef chef;
+	
+	public Village(String nom,int nbvillageoisMaximum) {
+	this.nom = nom;
+	}
+	public void setChef(Chef chef) {
+	this.chef = chef;
+	}
+	public String getNom() {
+	return nom;
+	}
+}
