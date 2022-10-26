c69a577a66ff15bdc349f49765622ec65b8c8ae6
Aissatou Barry <aissatoubarry62355@gmail.com>
2022-10-26 23:14:00 +0200

    Fin tp3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,57 +1,85 @@
 package personnages;
 
 public class Gaulois {
-	private static String nom;
+	private int effetPotion = 1;
+	private String nom;
+	// private int force;
 	private int force;
-	private static int effetPotion = 1;
+	private int nbTrophees;
+	private Equipement[] trophees = new Equipement[100];
 
 	public Gaulois(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
-		this.effetPotion = effetPotion;
 	}
 
 	public String getNom() {
 		return nom;
 	}
 
-	public void setForce(int force) {
-		this.force = force;
-	}
-
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "<<" + texte + ">>");
-
+		System.out.println(prendreParole() + " << " + texte + " >> ");
 	}
 
-	private static String prendreParole() {
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+
+	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
 	}
 
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+//		romain.recevoirCoup(force/ 3)*effetPotion;
+//	}
+
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3 * effetPotion);
+		Equipement[] tropheesGagnes = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; tropheesGagnes != null && i < tropheesGagnes.length; i++, nbTrophees++) {
+			trophees[nbTrophees] = tropheesGagnes[i];
 
+		}
 	}
 
-	public static void boirePotion(int i) {
-		effetPotion = i;
-
-		System.out.println(prendreParole() + "Merci Druide, je sens que ma force est " + i + " fois dupliquée.");
-
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		parler("merci Druide je sens que ma force est " + effetPotion + "fois decuplée ");
 	}
 
-//    @Override
-//    public String toString() {
-//        return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-//    }
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + "]";
+	}
+
+	public void faireUneDonnation(Musee musee){
+
+		if (trophees != null) {
+			StringBuilder bld = new StringBuilder("Je donne au musee tous mes trophees:");
+			
+			for (int i = 0; i < nbTrophees; i++) {
+				bld.append("\n-" + trophees[i].toString());
+				musee.donnerTrophees(this, trophees[i]);
+			}
+			String texte = bld.toString();
+
+			parler(texte);
+		}
+			
+
+	}
+	
+
 
 	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Asterix", 8);
-		System.out.println(asterix.nom);
-		asterix.boirePotion(3);
 
+		Gaulois asterix = new Gaulois("asterix", 8);
+		Romain beta = new Romain("Beta", 6);
+		asterix.parler("Salut mariama");
+		System.out.println(asterix);
+		System.out.println("le gaulois " + asterix.getNom() + " vient d'atterir sur terre merci à tous.");
+		asterix.frapper(beta);
+		asterix.boirePotion(6);
 	}
-
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,35 +2,64 @@ package personnages;
 
 public class Village {
 	private Gaulois[] villageois;
-	private  int nbvillageois = 0;
+	private int nbVillageois = 0;
 	private String nom;
 	private Chef chef;
-	
-	public Village(String nom,int nbvillageoisMaximum) {
+
+	public Village(String nom, int nbVillageoisMaximum) {
+		villageois = new Gaulois[nbVillageoisMaximum];
 		this.nom = nom;
-		villageois = new Gaulois[nbvillageoisMaximum];
 	}
-	
-	public void ajouterHabitant(Gaulois  gauloisAAjouter) {
-		if (nbvillageois < villageois.length){
-			villageois[nbvillageois] = gauloisAAjouter;
-				nbvillageois++;
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void ajouterHabitant(Gaulois gauloisAAjouter) {
+		if (nbVillageois < villageois.length) {
+			villageois[nbVillageois] = gauloisAAjouter;
+			nbVillageois++;
+		}
+
+	}
+
+	public Gaulois trouverHabitant(int numVillageois) {
+		return villageois[numVillageois];
+
+	}
+
+	public void afficherVillageois() {
+		String info = "Dans village du chef " + chef.getNom() + " vivent les legendaires gaulois:";
+
+		System.out.println(info);
+		for (int i = 0; i < nbVillageois; i++) {
+			System.out.println(villageois[i].getNom());
+
 		}
 	}
-	
-	public Gaulois trouverHabitant(int numVillageois) {
-		Gaulois gaulois =villageois[numVillageois];
-		 return gaulois;
-	}
-	
-	public void setChef(Chef chef) {
-	this.chef = chef;
-	}
-	
-	public String getNom() {
-	return nom;
-	}
+
 	public static void main(String[] args) {
-		
+		// Gaulois gaulois = village.trouverHabitant(30);
+		Village village = new Village("Village des Irréductibles", 30);
+		// Gaulois gaulois = village.trouverHabitant(30)
+		// impossible car l'index 30 depasse le nb villageois maximum
+
+		Chef chef = new Chef("Abraracourcix", 6, village);
+		village.setChef(chef);
+		Gaulois asterix = new Gaulois("asterix", 8);
+		village.ajouterHabitant(asterix);
+		// Gaulois gaulois = village.trouverHabitant(1);
+		// System.out.println(gaulois);
+		// La reponse affiché est null car il ya pas de gaulois dans ce village à la
+		// position 1
+		Gaulois obélix = new Gaulois("obélix", 25);
+		village.ajouterHabitant(obélix);
+		village.afficherVillageois();
+
 	}
+
 }
